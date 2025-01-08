ee472f3fbeae664dbbcd3bf923b031bb78f93427
Ashish1815 <ashishshinde485@gmail.com>
2025-01-08 17:53:51 +0530

    change

--- src/Pages/Table.jsx
+++ src/Pages/Table.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState, useRef } from "react";
+import { useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -7,104 +7,110 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-import Slide from "@mui/material/Slide";
-import { styled } from "@mui/material/styles";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
-import TextField from "@mui/material/TextField";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import { FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
+import TablePagination from "@mui/material/TablePagination";
 import EditIcon from "@mui/icons-material/Edit";
-import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
-import GridViewIcon from "@mui/icons-material/GridView";
+import DeleteIcon from "@mui/icons-material/Delete";
 import AutorenewIcon from "@mui/icons-material/Autorenew";
 
 const CustTable = (props) => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  // Calculate the rows to display based on the current page and rows per page
+  const rowsToDisplay = props.filteredRows.slice(
+    page * rowsPerPage,
+    page * rowsPerPage + rowsPerPage
+  );
+
   return (
     <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Mobile</TableCell>
-              <TableCell>Address</TableCell>
-              <TableCell>Joing Date</TableCell>
-              <TableCell>Remaining Amount</TableCell>
-              <TableCell>Memberships</TableCell>
-              <TableCell>Batch</TableCell>
-              <TableCell>Renew Memberships</TableCell>
-              <TableCell>Action</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {props.filteredRows.length === 0 ? (
+      <div className="tableDiv">
+        <TableContainer component={Paper} sx={{ maxHeight: "388px" }}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
               <TableRow>
-                <TableCell colSpan={8} align="center">
-                  Record not found
-                </TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Mobile</TableCell>
+                <TableCell>Address</TableCell>
+                <TableCell>Joining Date</TableCell>
+                <TableCell>Remaining Amount</TableCell>
+                <TableCell>Memberships</TableCell>
+                <TableCell>Batch</TableCell>
+                <TableCell>Renew Memberships</TableCell>
+                <TableCell>Action</TableCell>
               </TableRow>
-            ) : (
-              props.filteredRows.map((row, index) => (
-                <TableRow key={index}>
-                  <TableCell className="tableImg">
-                    <img className="me-3" src={row.img} alt="img" />
-                    {row.fristName + row.lastName}
-                  </TableCell>
-                  <TableCell>{row.mobileNo}</TableCell>
-                  <TableCell>{row.addresses}</TableCell>
-                  <TableCell>{row.joingDate}</TableCell>
-                  <TableCell>{row.remainingAmount}</TableCell>
-                  <TableCell>{row.memberships}</TableCell>
-                  <TableCell>{row.batch}</TableCell>
-                  <TableCell>{row.renew}</TableCell>
-                  <TableCell>
-                    {props.op === "customer" ? (
-                      <>
-                        <EditIcon
-                          className="me-2"
-                          onClick={() => props.onHadelClick(row, "Update")}
-                        />
-                        <DeleteIcon
-                          className="me-2"
-                          onClick={(e) => {
-                            onHadelDelete(row, "Delete");
-                          }}
-                        />
-                      </>
-                    ) : props.op === "overdue" ? (
-                      <>
-                        <AutorenewIcon
-                          className="me-2"
-                          onClick={(e) => {
-                            onHadelDelete(row, "Renewal");
-                          }}
-                        />
-                        <DeleteIcon
-                          className="me-2"
-                          onClick={(e) => {
-                            onHadelDelete(row, "Delete");
-                          }}
-                        />
-                      </>
-                    ) : null}
+            </TableHead>
+            <TableBody>
+              {rowsToDisplay.length === 0 ? (
+                <TableRow>
+                  <TableCell colSpan={9} align="center">
+                    Record not found
                   </TableCell>
                 </TableRow>
-              ))
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
+              ) : (
+                rowsToDisplay.map((row, index) => (
+                  <TableRow key={index}>
+                    <TableCell className="tableImg">
+                      <img className="me-3" src={row.img} alt="img" />
+                      {row.fristName + row.lastName}
+                    </TableCell>
+                    <TableCell>{row.mobileNo}</TableCell>
+                    <TableCell>{row.addresses}</TableCell>
+                    <TableCell>{row.joingDate}</TableCell>
+                    <TableCell>{row.remainingAmount}</TableCell>
+                    <TableCell>{row.memberships}</TableCell>
+                    <TableCell>{row.batch}</TableCell>
+                    <TableCell>{row.renew}</TableCell>
+                    <TableCell>
+                      {props.op === "customer" ? (
+                        <>
+                          <EditIcon
+                            className="me-2"
+                            onClick={() => props.onHadelClick(row, "Update")}
+                          />
+                          <DeleteIcon
+                            className="me-2"
+                            onClick={() => props.onHadelDelete(row, "Delete")}
+                          />
+                        </>
+                      ) : props.op === "overdue" ? (
+                        <>
+                          <AutorenewIcon
+                            className="me-2"
+                            onClick={() => props.onHadelDelete(row, "Renewal")}
+                          />
+                          <DeleteIcon
+                            className="me-2"
+                            onClick={() => props.onHadelDelete(row, "Delete")}
+                          />
+                        </>
+                      ) : null}
+                    </TableCell>
+                  </TableRow>
+                ))
+              )}
+            </TableBody>
+          </Table>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 15]}
+            component="div"
+            count={props.filteredRows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </TableContainer>
+      </div>
     </>
   );
 };
