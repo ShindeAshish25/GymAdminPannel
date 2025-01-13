936e2ac17d81c34fa4b82da8cd0a32e9f9bafe43
Ashish1815 <ashishshinde485@gmail.com>
2025-01-13 18:53:28 +0530

    changea

--- src/Pages/NavBar.jsx
+++ src/Pages/NavBar.jsx
@@ -13,13 +13,33 @@ import MailIcon from "@mui/icons-material/Mail";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
+import Modal from "@mui/material/Modal";
+import { Close } from "@mui/icons-material";
 
 const pages = ["Customer", "Overdue Memberships", "Old Memberships"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 600,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+};
 function Navbar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
+  const [data, setData] = React.useState("");
+
+  const handleOpen = (data) => {
+    console.log(data);
+    setData(data);
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -36,6 +56,73 @@ function Navbar() {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+
+  const alertData = [
+    {
+      id: "1",
+      img: "https://codingyaar.com/wp-content/uploads/bootstrap-profile-card-image.jpg",
+      name: "Ashish Shinde",
+      mobNo: "9049831815",
+      lastPaymetDate: "27-12-2024",
+      lastMembership: "1 month",
+      membershipDueDate: "27-12-2024",
+    },
+    {
+      id: "2",
+      img: "https://codingyaar.com/wp-content/uploads/bootstrap-profile-card-image.jpg",
+      name: "Yogesh Shinde",
+    },
+    {
+      id: "3",
+      img: "https://codingyaar.com/wp-content/uploads/bootstrap-profile-card-image.jpg",
+      name: "Sunny Shinde",
+    },
+    {
+      id: "1",
+      img: "https://codingyaar.com/wp-content/uploads/bootstrap-profile-card-image.jpg",
+      name: "Ashish Shinde",
+    },
+    {
+      id: "2",
+      img: "https://codingyaar.com/wp-content/uploads/bootstrap-profile-card-image.jpg",
+      name: "Yogesh Shinde",
+    },
+    {
+      id: "3",
+      img: "https://codingyaar.com/wp-content/uploads/bootstrap-profile-card-image.jpg",
+      name: "Sunny Shinde",
+    },
+    {
+      id: "1",
+      img: "https://codingyaar.com/wp-content/uploads/bootstrap-profile-card-image.jpg",
+      name: "Ashish Shinde",
+    },
+    {
+      id: "2",
+      img: "https://codingyaar.com/wp-content/uploads/bootstrap-profile-card-image.jpg",
+      name: "Yogesh Shinde",
+    },
+    {
+      id: "3",
+      img: "https://codingyaar.com/wp-content/uploads/bootstrap-profile-card-image.jpg",
+      name: "Sunny Shinde",
+    },
+    {
+      id: "1",
+      img: "https://codingyaar.com/wp-content/uploads/bootstrap-profile-card-image.jpg",
+      name: "Ashish Shinde",
+    },
+    {
+      id: "2",
+      img: "https://codingyaar.com/wp-content/uploads/bootstrap-profile-card-image.jpg",
+      name: "Yogesh Shinde",
+    },
+    {
+      id: "3",
+      img: "https://codingyaar.com/wp-content/uploads/bootstrap-profile-card-image.jpg",
+      name: "Sunny Shinde",
+    },
+  ];
 
   return (
     <>
@@ -143,9 +230,9 @@ function Navbar() {
             </Box>
 
             <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
+              <Tooltip title="Over Due Memberships customer">
                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Badge badgeContent={10} color="error">
+                  <Badge badgeContent={alertData.length} color="error">
                     <MailIcon sx={{ color: "white" }} color="action" />
                   </Badge>
                 </IconButton>
@@ -154,6 +241,7 @@ function Navbar() {
               <Menu
                 sx={{ mt: "45px" }}
                 id="menu-appbar"
+                className="alertBox"
                 anchorEl={anchorElUser}
                 anchorOrigin={{
                   vertical: "top",
@@ -167,11 +255,23 @@ function Navbar() {
                 open={Boolean(anchorElUser)}
                 onClose={handleCloseUserMenu}
               >
-                {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                    <Typography sx={{ textAlign: "center" }}>
-                      {setting}
-                    </Typography>
+                {alertData.map((alertData, index) => (
+                  <MenuItem
+                    className=""
+                    key={index}
+                    // onClick={handleCloseUserMenu}
+                    onClick={() => {
+                      handleCloseUserMenu();
+                      handleOpen(alertData);
+                    }}
+                  >
+                    {/* {setting} */}
+                    <div className="d-flex align-items-center border-bottom pb-2 ">
+                      <div className="alertImg">
+                        <img src={alertData.img} alt="" />
+                      </div>
+                      <div className="alertText ms-3">{alertData.name}</div>
+                    </div>
                   </MenuItem>
                 ))}
               </Menu>
@@ -180,7 +280,59 @@ function Navbar() {
         </Container>
       </AppBar>
 
-      <div></div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        className="alertCard"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-description">
+            <div className="row">
+              <div className="col-md-6 ">
+                <img src={data.img} alt="" />
+              </div>
+              <div className="col-md-6">
+                <Typography
+                  className="mb-3"
+                  id="modal-modal-title"
+                  variant="h4"
+                  component="h4"
+                >
+                  {data.name}
+                </Typography>
+
+                <p>
+                  <b className="me-3">Last Paymet Date :</b>
+                  {data.lastPaymetDate}
+                </p>
+                <p>
+                  <b className="me-3">Last Membership :</b>
+                  {data.lastMembership}
+                </p>
+                <p>
+                  <b className="me-3"> Due Date:</b>
+                  {data.membershipDueDate}
+                </p>
+                <p>
+                  <b className="me-3">Mobile No :</b>
+                  {data.mobNo}
+                </p>
+                <div className="text-end mt-3">
+                  <Button
+                    variant="outlined"
+                    color="error"
+                    onClick={handleClose}
+                  >
+                    Close
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </Typography>
+        </Box>
+      </Modal>
     </>
   );
 }
