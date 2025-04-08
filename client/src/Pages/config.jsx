6382aeaf4d33c1160a0bc53d2855529e4415d87a
mohsinalli-sayyad <mohsinallisayyad72@gmail.com>
2025-04-08 22:43:36 +0530

    Stop tracking server/.env

--- client/src/Pages/config.jsx
+++ client/src/Pages/config.jsx
@@ -1,1 +1,1 @@
-export const baseURL = `http://localhost:5000/api/customers`;
+export const baseURL = (process.env.NODE_ENV === 'production') ? "https://api.o3fitness.in/api/customers" : `http://localhost:5000/api/customers`;
